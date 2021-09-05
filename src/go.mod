fe12ae26cbf02e75ddd678f92766700e2cd5895b
murata0531 <coless115@gmail.com>
2021-09-06 05:10:45 +0900

    不要なコミットを削除

---
+++ src/go.mod
@@ -0,0 +1,8 @@
+module github.com/murata0531/Network-boot
+
+go 1.13
+
+require (
+	go.universe.tf/netboot v0.0.0-20200205210610-68743c67a60c
+	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4
+)
---
+++ src/tftp.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"go.universe.tf/netboot/tftp"
+)
+
+type TFTPServer struct {
+	TFTPBootDir string
+
+	conn   net.PacketConn
+	closed bool
+	m      sync.Mutex
+}
+
+func (s *TFTPServer) Start(listen string) error {
+	srv := &tftp.Server{Handler: s.handle}
+
+	log.Printf("[INFO] Starting TFTP server on %s ...", listen)
+
+	var err error
+	s.conn, err = net.ListenPacket("udp4", listen)
+	if err != nil {
+		return err
+	}
+	err = srv.Serve(s.conn)
+	if err != nil {
+		s.m.Lock()
+		if s.closed {
+			err = nil
+		}
+		s.m.Unlock()
+	}
+	return err
+}
+
+func (s *TFTPServer) Shutdown() error {
+	s.m.Lock()
+	s.closed = true
+	s.m.Unlock()
+
+	return s.conn.Close()
+}
+
+func (s *TFTPServer) handle(path string, addr net.Addr) (io.ReadCloser, int64, error) {
+	log.Printf("[INFO] GET %s from %s", path, addr)
+
+	f, err := os.Open(filepath.Join(s.TFTPBootDir, path))
+	if err != nil {
+		log.Printf("[ERROR] %v", err)
+		return nil, 0, err
+	}
+	fi, err := f.Stat()
+	if err != nil {
+		log.Printf("[ERROR] %v", err)
+		return nil, 0, err
+	}
+	return f, fi.Size(), err
+}
